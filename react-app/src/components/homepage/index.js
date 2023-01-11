8ed0bde5861652847a7b5f73fe9e13487adc690e
JB Kam <jb3.kam@gmail.com>
2023-01-10 22:01:28 -0800

    updated some css to mainpage

--- react-app/src/components/homepage/index.js
+++ react-app/src/components/homepage/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './homepage.css'
 
 function HomePage() {
 
 
     return (
         <>
-            <h1> Here is a collection of my mini projects: </h1>
-            <div>
-                <NavLink to={'/weather'}>
-                    <div> Click Here for Weather App!</div>
-                </NavLink>
+            <h1 style={{ display: "flex", justifyContent: 'center' }}> Here is a collection of my mini projects: </h1>
+            <div className='home'>
+                <div className='mainpage-template'>
+                    <div className='project-container'>
+                        <NavLink to={'/weather'}>
+                            <div> Weather App</div>
+                        </NavLink>
+                    </div>
+                    <div className='project-container'>
+                        chatgpt api
+                    </div>
+                    <div className='project-container'>
+                        simple calculator
+                    </div>
+                    <div className='project-container'>
+                        something else
+                    </div>
+                </div>
             </div>
-            <div>
-                chatgpt api
-            </div>
-            <div>
-                simple calculator
-            </div>
-
         </>
     )
 
