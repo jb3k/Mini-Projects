cd993c5d1359bd1c3d9e3696d9e868b7db057881
JB Kam <jb3.kam@gmail.com>
2023-02-20 19:40:15 -0800

    express refresher

---
+++ react-app/src/components/nodeJS/practice/express.js
@@ -0,0 +1,30 @@
+//always have to import
+const express = require('express')
+
+//need to call for express app
+const app = express()
+
+//listen for requests
+app.listen(3000)
+
+app.get('/', (req, res) => {
+
+    //essentially the same as res.setHeader() which works out what content type we send out and also statuscode
+    res.send();
+
+})
+
+
+//redirects
+app.get('/', (req, res) => {
+    //redirect method with url as param
+    res.redirect('/about')
+
+})
+
+//404page
+//use will run if there are no other matches, so needs to be at the bottom
+app.use((req, res) => {
+
+    res.sendFile('/file path', { root: __dirname })
+})
