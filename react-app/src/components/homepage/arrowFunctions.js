4b1d562b88abe2f37508616b4e50cd7ce9996213
JB Kam <jb3.kam@gmail.com>
2023-03-01 17:42:27 -0800

    fixed the slick css stuff for the arrows

---
+++ react-app/src/components/homepage/arrowFunctions.js
@@ -0,0 +1,21 @@
+export function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block" }}
+            onClick={onClick}
+        />
+    );
+}
+
+export function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block" }}
+            onClick={onClick}
+        />
+    );
+}
--- react-app/src/components/homepage/index.js
+++ react-app/src/components/homepage/index.js
@@ -2,50 +2,58 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './homepage.css'
 import Slider from "react-slick";
-
+import { SampleNextArrow, SamplePrevArrow } from './arrowFunctions';
 import "./slick.css";
 import "./slick-theme.css";
 
 function HomePage() {
 
-
     const settings = {
-        dots: true,
+        className: "center",
+        centerMode: true,
         infinite: true,
+        centerPadding: "100px",
+        slidesToShow: 1,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+        dots: true,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+        // infinite: true,
+        // speed: 500,
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2,
+        //             initialSlide: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
     }
 
     return (
         <div className="slider-container">
             <h2 className="homepage-title"> Here is a Collection of my Mini projects</h2>
+            <h2> APIS: </h2>
             <Slider {...settings}>
                 <div className='project-container'>
                     <NavLink to={'/weather'}>
@@ -53,10 +61,24 @@ function HomePage() {
                     </NavLink>
                 </div>
                 <div className='project-container'>
+                    In Progress
+                </div>
+            </Slider>
+
+            <h2> Mini-Games: </h2>
+            <Slider {...settings}>
+                <div className='project-container'>
                     <NavLink to={'/tictactoe'}>
                         <div> TicTacToe Game</div>
                     </NavLink>
                 </div>
+                <div className='project-container'>
+                    In Progress
+                </div>
+            </Slider>
+
+            <h2> Tools: </h2>
+            <Slider {...settings}>
                 <div className='project-container'>
                     <NavLink to={'/calc'}>
                         <div>
@@ -65,9 +87,8 @@ function HomePage() {
                     </NavLink>
                 </div>
                 <div className='project-container'>
-                    something else
+                    In Progress
                 </div>
-                {/* </div> */}
             </Slider>
         </div>
     );
