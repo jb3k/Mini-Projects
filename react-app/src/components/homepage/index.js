40060a411ce7f2f30862c69cd572d064f1ef2537
JB Kam <jb3.kam@gmail.com>
2023-03-01 16:40:53 -0800

    added react slick

--- react-app/src/components/homepage/index.js
+++ react-app/src/components/homepage/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './homepage.css'
+import SliderComponent from './slider';
 
 function HomePage() {
 
 
     return (
-        <>
+        <div className='body'>
             <h1 style={{ display: "flex", justifyContent: 'center' }}> Here is a collection of my mini projects: </h1>
             <div className='home'>
                 <div className='mainpage-template'>
@@ -32,7 +33,31 @@ function HomePage() {
                     </div>
                 </div>
             </div>
-        </>
+
+            <div className='container'>
+                <div className='slider'>
+                    <img src='https://via.placeholder.com/210?text=1' />
+                    <img src='https://via.placeholder.com/220?text=2' />
+                    <img src='https://via.placeholder.com/230?text=3' />
+                    <img src='https://via.placeholder.com/240?text=4' />
+                    <img src='https://via.placeholder.com/250?text=5' />
+                    <img src='https://via.placeholder.com/260?text=6' />
+                    <img src='https://via.placeholder.com/270?text=7' />
+                    <img src='https://via.placeholder.com/280?text=8' />
+                    <img src='https://via.placeholder.com/290?text=9' />
+
+
+
+                </div>
+            </div>
+
+            <div>
+                <SliderComponent />
+            </div>
+
+
+
+        </div>
     )
 
 };
